f66f94ebec982d02a148803d3ca4e299b1cbdf8c
remilebeau <remilebeau90@gmail.com>
2024-05-09 15:05:22 -0400

    added instructions

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,20 @@ function App() {
   const solution = fetchSolution();
 
   return (
-    <main className="max-w-4xl mx-auto flex flex-col gap-4 justify-center items-center">
+    <main className="max-w-4xl mx-auto flex flex-col gap-4 justify-center items-center mt-8">
       <h1 className="text-5xl font-bold">Wordle</h1>
+      <h2 className="text-2xl font-bold">Enter a guess and press Enter</h2>
+      <p className="text-2xl font-bold">Refresh the page for a new puzzle</p>
+      <section id="instructions" className="flex flex-row gap-4">
+        <p className="text-2xl text-green-500 font-bold">
+          Correct letter and place
+        </p>
+        <p className="text-2xl text-yellow-500 font-bold">
+          Correct letter, wrong place
+        </p>
+        <p className="text-2xl text-gray-500 font-bold">Incorrect letter</p>
+      </section>
+
       {solution && <Wordle solution={solution} />}
     </main>
   );
