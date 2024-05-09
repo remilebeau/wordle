090eeb83adf6664d413bbd0040a390a73e062617
remilebeau <remilebeau90@gmail.com>
2024-05-08 21:20:15 -0400

    attempting to fix deployment issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import Wordle from "./components/Wordle";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [errMsg, setErrMsg] = useState("");
   const [solution, setSolution] = useState("");
 
   const fetchSolution = async () => {
     const res = await fetch(
       "https://wordle-backend-od4f.onrender.com/api/solutions"
     );
-    if (!res.ok) {
-      setErrMsg("Failed to fetch solution");
-    }
     const { solution } = await res.json();
     setSolution(solution);
   };
@@ -23,7 +19,6 @@ function App() {
   return (
     <main className="max-w-4xl mx-auto flex flex-col justify-center items-center">
       <h1>Wordle</h1>
-      {errMsg && <p>{errMsg}</p>}
       {solution && <Wordle solution={solution} />}
     </main>
   );
