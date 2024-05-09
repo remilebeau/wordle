300e6b452e05df9d2e2e107340ffb264c771db69
remilebeau <remilebeau90@gmail.com>
2024-05-08 21:06:07 -0400

    added error handling to fetchSolution

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import Wordle from "./components/Wordle";
 import { useState, useEffect } from "react";
 
 function App() {
+  const [errMsg, setErrMsg] = useState("");
   const [solution, setSolution] = useState("");
 
   const fetchSolution = async () => {
     const res = await fetch(
       "https://wordle-backend-od4f.onrender.com/api/solutions"
     );
+    if (!res.ok) {
+      setErrMsg("Failed to fetch solution");
+    }
     const data = await res.json();
     setSolution(data.word);
   };
@@ -18,8 +22,8 @@ function App() {
 
   return (
     <main className="max-w-4xl mx-auto flex flex-col justify-center items-center">
-      <h1>Hello World</h1>
-      <h2>Solution is {solution}</h2>
+      <h1>Wordle</h1>
+      {errMsg && <p>{errMsg}</p>}
       {solution && <Wordle solution={solution} />}
     </main>
   );
